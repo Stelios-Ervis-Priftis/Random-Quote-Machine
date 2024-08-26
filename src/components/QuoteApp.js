5d2e7ba812e95bb7caa198be86a2b61bbf315a46
Stelios Ervis Prifits <steliospriftis@gmail.com>
2024-08-27 00:38:15 +0200

    Replace old Quote restfull api with new

--- src/components/QuoteApp.js
+++ src/components/QuoteApp.js
@@ -1,75 +1,79 @@
-import React, { Component } from 'react'
-import randomColor from 'random-color'
-import TypeIt from 'typeit-react'
+import randomColor from 'random-color';
+import React, { Component } from 'react';
+import 'regenerator-runtime/runtime';
 
 export default class QuoteApp extends Component {
 	state = {
 		quote: [],
-		color: null,
-	}
-	fetchQuoteApi = () => {
-		fetch('https://type.fit/api/quotes')
-			.then((res) => res.json())
-			.then((result) => {
-				const randomNum = Math.floor(Math.random() * result.length)
-				const quote = result[randomNum]
-				this.setState({
-					quote: quote,
-					color: randomColor().hexString(),
-				})
-			})
-	}
+		color: randomColor().hexString(),
+	};
+
+	fetchQuoteApi = async () => {
+		const url = 'https://api.api-ninjas.com/v1/quotes';
+
+		try {
+			const response = await fetch(url, {
+				method: 'GET',
+				headers: {
+					'X-Api-Key': process.env.NINJA_KEY,
+					contentType: 'application/json',
+				},
+			});
+
+			if (!response.ok) {
+				throw new Error(`Response status: ${response.status}`);
+			}
+
+			const quote = await response.json();
+
+			this.setState({
+				quote: quote,
+				color: randomColor().hexString(),
+			});
+		} catch (error) {
+			console.error(error.message);
+		}
+	};
+
 	componentDidMount() {
-		this.fetchQuoteApi()
+		this.fetchQuoteApi();
 	}
+
 	getNewQuote = () => {
-		this.fetchQuoteApi()
-	}
+		this.fetchQuoteApi();
+	};
+
 	render() {
 		const randomColor = {
 			color: this.state.color,
-		}
-		console.log(this.state.quote)
-		console.log(this.state.color)
+		};
+
 		return (
-			<div className='container'>
-				<h1 className='title' style={randomColor}>
+			<div className="container">
+				<h1 className="title" style={randomColor}>
 					Random Quote Machine
 				</h1>
 
-				{this.state.quote.text ? (
-					<p className='quote' style={randomColor}>
-						{`"${this.state.quote.text}"`}
-					</p>
-				) : (
-					// <TypeIt
-					// 	options={{
-					// 		strings: this.state.quote.text,
-					// 		speed: 10,
-					// 		waitUntilVisible: true,
-					// 	}}
-					// />
-					<p className='quote' style={randomColor}>
-						Unknown Text
-					</p>
-				)}
+				{this.state.quote.map((q) => (
+					<div>
+						<p className="quote" style={randomColor}>
+							{`"${q.quote}"`}
+						</p>
 
-				{this.state.quote.author ? (
-					<p className='author' style={randomColor}>
-						{this.state.quote.author}
-					</p>
-				) : (
-					<p style={randomColor}>Unknown Author</p>
-				)}
+						<p className="author" style={randomColor}>
+							{q.author}
+						</p>
+					</div>
+				))}
+
 				<button
-					className='btn'
-					// style={{ color: `${randomColor}` }}
+					className="btn"
 					style={randomColor}
 					onClick={this.getNewQuote}
 				>
 					New Quote
 				</button>
 			</div>
-		)
+		);
 	}
 }
